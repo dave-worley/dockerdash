706128e1d1ea016b20fafd0c773a0db67439e8ee
David Worley <davidworley@gmail.com>
2015-12-29 19:18:17 -0800

    Formatting.

--- src/repoListService.js
+++ src/repoListService.js
@@ -3,15 +3,17 @@ angular.module('DockerDash')
 
         /*
 
-        This service would be replaced by actual API calls in production.
+         This service would be replaced by actual API calls in production.
 
-        */
+         */
 
         var makeRandomRepos, repos;
 
         makeRandomRepos = function () {
-            var repoList = [];
-            var random = function(min, max) { // borrowed from underscore
+            var repoList, random, description, accountNames, repoNames;
+
+            repoList = [];
+            random = function (min, max) { // borrowed from underscore
                 if (max == null) {
                     max = min;
                     min = 0;
@@ -19,16 +21,16 @@ angular.module('DockerDash')
                 return min + Math.floor(Math.random() * (max - min + 1));
             };
 
-            var description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum neque ornare nulla consequat, sit amet sodales purus cursus. Donec sed suscipit augue.';
-            var accountNames = ['test-user', 'other-user', 'best-user', 'dave'];
-            var repoNames = ['a-cool-repo', 'another-cool-repo', 'best-repo-evar', 'look-ma-more-code', 'never-updated-repo'];
+            description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum neque ornare nulla consequat, sit amet sodales purus cursus. Donec sed suscipit augue.';
+            accountNames = ['test-user', 'other-user', 'best-user', 'dave'];
+            repoNames = ['a-cool-repo', 'another-cool-repo', 'best-repo-evar', 'look-ma-more-code', 'never-updated-repo'];
 
             for (var i = 0; i < 100; i++) { // 100 total repositories
                 repoList.push({
                     account: accountNames[random(0, accountNames.length - 1)],
                     name: repoNames[random(0, repoNames.length - 1)],
-                    description: random(0,1) === 0 ? description : '', // 50% chance to have a description
-                    private: random(1,10) <= 3 ? true : false // 30% chance to be private
+                    description: random(0, 1) === 0 ? description : '', // 50% chance to have a description
+                    private: random(1, 10) <= 3 ? true : false // 30% chance to be private
                 });
             }
 
